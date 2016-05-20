bc4983644a4ecdee4f5da9b7ebd62e855074c1aa
Alam Arias <Alam.GBC@gmail.com>
2016-05-20 01:41:29 -0400

    checkjson: delete the fat, more human readable

--- checkjson.py
+++ checkjson.py
@@ -4,17 +4,15 @@ import simplejson
 import sys
 
 invalid_json_files = []
-read_json_files = []
 os.chdir(os.getcwd() + "/json")
 for files in os.listdir(os.getcwd()):
     with open(files) as json_file:
         try:
             simplejson.load(json_file)
-            read_json_files.append(files)
         except ValueError, e:
             print ("%s: %s") % (files, e)
             invalid_json_files.append(files)
 
 if len(invalid_json_files) != 0:
-    sys.exit("=============\nFound Errors in JSON files")
+    sys.exit("=============\nJSON files with issues: %d" % len(invalid_json_files))
 
