6a762fcc20bfdf9bb46adce9e7ad64a09c4c8247
Alam Arias <alam+github_public@srb2.org>
2017-04-23 18:30:43 -0400

    Travis: add check for dup item names

---
+++ dupcheck.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding=utf8
+import codecs
+import re
+import os
+import pprint
+import simplejson
+import sys
+
+os.chdir(os.getcwd() + "/json")
+ENMap = dict()
+json_files = [f for f in os.listdir('./') if re.match(r'Item_.*\.txt', f)]
+for files in json_files:
+    with codecs.open(files, mode='r', encoding='utf-8') as json_file:
+        try:
+            djson = simplejson.load(json_file)
+            for rmid in djson:
+                if (("tr_text" in rmid) and (rmid["tr_text"] != "")):
+                    #print(rmid["tr_text"])
+                    if rmid["tr_text"] not in ENMap:
+                        ENMap[rmid["tr_text"]] = rmid["jp_text"]
+                    elif ENMap[rmid["tr_text"]] == rmid["jp_text"]:
+                        print("DUP")
+                    else:
+                        print("%s: %s" % (files, rmid["assign"]))
+        except ValueError as e:
+            print("%s: %s") % (files, e)
