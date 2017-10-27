b4a09c32ee2e9e084f076266cdb8cf475b793681
Alam Arias <Alam.GBC@gmail.com>
2017-10-27 00:58:02 -0400

    Importer: add Name_Actor_MagName.txt

--- import.py
+++ import.py
@@ -61,6 +61,12 @@ names_file = [
     for f in fnmatch.filter(files, 'Name_UICharMake_*.txt')
 ]
 
+names_file += [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'Name_Actor_MagName.txt')
+]
+
 for files in explain_files:
     with codecs.open(files, mode='r', encoding='utf-8') as json_file:
         change = False
