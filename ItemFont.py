f5c194579eb2c01ba952ad62dbf513b3874e4fdc
Alam Arias <Alam.GBC@gmail.com>
2017-10-29 14:09:11 -0400

    Sort the item list by text, then by size

--- ItemFont.py
+++ ItemFont.py
@@ -60,11 +60,12 @@ for files in explain_files:
                 t = entry["jp_text"]
             FS[t] = _fonts.itemlength(t)
 
+FSk = OrderedDict(sorted(FS.items(), key=lambda t: t[0]))
+FSs = OrderedDict(sorted(FSk.items(), key=lambda t: t[1]))
+
 if len(sys.argv) == 3:
-    FSk = OrderedDict(sorted(FS.items(), key=lambda t: t[0]))
-    FSs = OrderedDict(sorted(FSk.items(), key=lambda t: t[1]))
     print(json.dumps(FSs, ensure_ascii=False, indent="\t", sort_keys=False))
 else:
-    for e in FS:
+    for e in FSs:
         if FS[e] > 29:
             print("Item Name {} is too big: {}".format(e, FS[e]))
