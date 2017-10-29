8ddd9d4b6af4127eb78c8bb09da47e0dd13821d8
Alam Arias <Alam.GBC@gmail.com>
2017-10-29 00:27:56 -0400

    New script to check the length of items

---
+++ ItemFont.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# coding=utf8
+import _fonts
+import codecs
+import fnmatch
+import json
+import os
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit(os.EX_NOINPUT)
+
+dir = sys.argv[1]
+
+FS = dict()
+
+explain_files = [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'Item_*.txt')
+]
+
+explain_files += [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'Explain_Actor_*.txt')
+]
+
+explain_files += [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'Explain_SkillRing.txt')
+]
+
+explain_files += [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'Explain_System.txt')
+]
+
+_fonts.init()
+
+for files in explain_files:
+    with codecs.open(files, mode='r', encoding='utf-8') as json_file:
+        djson = json.load(json_file)
+        for entry in djson:
+            if (entry["tr_text"] != ""):
+                t = entry["tr_text"]
+            else:
+                t = entry["jp_text"]
+            FS[t] = _fonts.itemlength(t)
+
+for e in FS:
+    if FS[e] > 27.4:
+        print("Item Name {} is too big: {}".format(e, FS[e]))
+
+#print(json.dumps(FS, ensure_ascii=False, indent="\t", sort_keys=False))
