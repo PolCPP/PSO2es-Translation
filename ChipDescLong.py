3f79c481919e2c5198f0e080177c7e47511e4b84
Alam Arias <Alam.GBC@gmail.com>
2017-11-02 20:30:22 -0400

    Checks: set a limit of ChipExplain longs

---
+++ ChipDescLong.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# coding=utf8
+import _fonts
+import codecs
+import fnmatch
+import json
+import os
+import sys
+from collections import OrderedDict
+
+if len(sys.argv) < 2:
+    sys.exit(os.EX_NOINPUT)
+
+dir = sys.argv[1]
+
+FS = dict()
+
+chip_files = [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(dir)
+    for f in fnmatch.filter(files, 'ChipExplain_*.txt')
+]
+
+if len(sys.argv) == 3 and sys.argv[2] != "0":
+    _fonts.init(int(sys.argv[2]))
+else:
+    _fonts.init()
+
+for files in chip_files:
+    with codecs.open(files, mode='r', encoding='utf-8') as json_file:
+        djson = json.load(json_file)
+        for entry in djson:
+            if (entry["tr_explainLong"] != ""):
+                t = entry["tr_explainLong"]
+            else:
+                t = entry["jp_explainLong"]
+            s = -1
+            for sl in t.splitlines():
+                s = max(_fonts.itemlength(sl), s)
+            FS[t] = s
+
+FSk = OrderedDict(sorted(FS.items(), key=lambda t: t[0]))
+FSs = OrderedDict(sorted(FSk.items(), key=lambda t: t[1]))
+
+if len(sys.argv) == 3:
+    print(json.dumps(FSs, ensure_ascii=False, indent="\t", sort_keys=False))
+else:
+    for e in FSs:
+        if FS[e] > 47:
+            print("Chip Long explain '{}' is too big: {}".format(e.replace("\n","<br>"), FS[e]))
