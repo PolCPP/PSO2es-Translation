1d7c32274eda898e0fcffd002829ecfb4f9a4d82
Alam Arias <Alam.GBC@gmail.com>
2017-11-02 20:05:38 -0400

    Revert "ItemImport: remove Dup tr_text"
    This reverts commit a05239d8572afbc1421bef0f73e9747d9a89ff4b.

--- ItemImport.py
+++ ItemImport.py
@@ -118,9 +118,6 @@ for files in explain_files:
                     TR_name[k] = t
                     TR_explain[k] = d
                     TR_src[k] = "NEW"
-                if t == k:
-                    TR_name[k] = ""
-                    change = True
             if change:
                 print("Updating {}.txt".format(
                     os.path.splitext(os.path.basename(files))[0]))
@@ -169,9 +166,6 @@ for files in names_file:
                     t = entry["tr_text"]
                     TR_name[k] = t
                     TR_src[k] = "NEW"
-                if k == t:
-                    TR_name[k] = ""
-                    change = True
             if change:
                 print("Updating {}.txt".format(
                     os.path.splitext(os.path.basename(files))[0]))
