083b77c5cdc73b17d214d32304d830a4a3061ee7
Alam Arias <Alam.GBC@gmail.com>
2017-11-08 20:48:29 -0500

    keep a blacklist for dupassign.py

--- dupassign.py
+++ dupassign.py
@@ -13,60 +13,34 @@ if len(sys.argv) < 2:
 
 dir = sys.argv[1]
 
+blacklist = list()
 json_files = [
     os.path.join(dirpath, f)
     for dirpath, dirnames, files in os.walk(dir)
     for f in fnmatch.filter(files, '*.txt')
 ]
 
-#  DragonMagmaEx
-nope_files = [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk(dir)
-    for f in fnmatch.filter(files, 'Name_Actor_Enemy.txt')
-]
+#  Name_Actor_Enemy.txt
+blacklist += ["DragonMagmaEx"]
+#  UI_Server.txt
+blacklist += ["313_t", "400_t", "221_t"]
+blacklist += ["313_b", "400_b", "221_b"]
+#  ChipExplain_ActiveExplain.txt
+blacklist += ["72060"]
+#  Name_Chip_ActiveName.txt
+blacklist += ["72020"]
+#  Name_UICharMake_AccessoryName.txt
+blacklist += ["No06421"]
+blacklist += ["No14429"]
+blacklist += ["No00090"]
 
-#  *_t
-#  *_b
-nope_files += [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk(dir)
-    for f in fnmatch.filter(files, 'UI_Server.txt')
-]
-
-#  72060
-nope_files += [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk(dir)
-    for f in fnmatch.filter(files, 'ChipExplain_ActiveExplain.txt')
-]
-
-#  72020
-nope_files += [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk(dir)
-    for f in fnmatch.filter(files, 'Name_Chip_ActiveName.txt')
-]
-
-#  No06421
-#  No14429
-#  No00090
-nope_files += [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk(dir)
-    for f in fnmatch.filter(files, 'Name_UICharMake_AccessoryName.txt')
-]
-
-assign_files = [x for x in json_files if x not in nope_files]
-
-for files in assign_files:
+for files in json_files:
     with codecs.open(files, mode='r', encoding='utf-8') as json_file:
         djson = json.load(json_file)
         assigns = dict()
-        #print(files)
         for entry in djson:
             if "assign" not in entry:
-                 continue
+                continue
             a = entry["assign"]
             if "tr_text" in entry:
                 t = entry["tr_text"]
@@ -79,13 +53,14 @@ for files in assign_files:
             else:
                 t = entry["text"]
             if a in assigns:
-                print("{}: {}:{}/{}".format(
-                    files,
-                    a,
-                    t,
-                    assigns[a]
-                ))
-                countdup = countdup+1
+                if a not in blacklist:
+                    print("{}: {}:{}/{}".format(
+                        files,
+                        a,
+                        t,
+                        assigns[a]
+                    ))
+                    countdup = countdup+1
             else:
                 assigns[a] = t
 
