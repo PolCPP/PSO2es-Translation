626de1166d3b34965a38bb63924e820ecce4fc17
Alam Arias <Alam.GBC@gmail.com>
2017-11-10 03:15:07 -0500

    normalize: more undoing

--- normalize.py
+++ normalize.py
@@ -8,7 +8,7 @@ import sys
 import unicodedata
 from collections import OrderedDict
 
-quick = {"*": "＊", "『": "\"", "』": "\"", "ō": "ou", "–": "-", "‒": "-", "​":"", "—": "-", "ー": "-", "ū": "uo"}
+quick = {"*": "＊", "『": "\"", "』": "\"", "ō": "ou", "–": "-", "‒": "-", "​":"", "ū": "uo"}
 
 # error counter
 countdup = 0
