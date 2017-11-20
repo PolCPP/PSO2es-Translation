dc2c9dbb0bce8c4eb7ccce08815f760467448a78
Alam Arias <Alam.GBC@gmail.com>
2017-11-20 02:02:03 -0500

    Scripts: add sh/batch files to roll out patches

---
+++ make.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Making patch
+ESBreakerCLI.exe
+echo Waiting for android device
+adb wait-for-device
+adb push output/. /storage/emulated/0/Android/data/com.sega.PhantasyStarOnline2es/files/3hwQzp8KE9T1oTpJCHPvxI5JIedD3AuT/
+adb shell am start -S com.sega.PhantasyStarOnline2es/.PSO2esUnityActivity
