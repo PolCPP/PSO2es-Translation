5ebb1a1bda6857847c3b7465d0241d0e2dde0cbf
Alam Arias <Alam.GBC@gmail.com>
2017-11-23 18:32:20 -0500

    Fixup make.sh

--- make.sh
+++ make.sh
@@ -6,4 +6,3 @@ echo Waiting for android device
 adb wait-for-device
 adb push ~/git/PSO2es-Translation/ESBreaker.Debug/output/. /storage/emulated/0/Android/data/com.sega.PhantasyStarOnline2es/files/3hwQzp8KE9T1oTpJCHPvxI5JIedD3AuT/
 adb shell am start -S com.sega.PhantasyStarOnline2es/.PSO2esUnityActivity
-adb logcat |grep com.sega.PhantasyStarOnline2es
