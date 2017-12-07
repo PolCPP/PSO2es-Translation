a66f0cf2a67ea779dae01614260984ce697fea90
Alam Arias <Alam.GBC@gmail.com>
2017-12-06 22:40:52 -0500

    Fixup update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 echo Waiting for android device
 adb wait-for-device
-pmapkpath=`adb shell pm path com.sega.PhantasyStarOnline2es`
-apkpath=$(echo $pmapkpath|_py/APKcut.py)
-adb pull $apkpath tmp
+apkpath=$(adb shell pm path com.sega.PhantasyStarOnline2es | _py/_APK_package.py)
+apkversion=$(adb shell dumpsys package com.sega.PhantasyStarOnline2es| _py/_APK_version.py)
+mkdir -p apk
+echo pulling PSO2 APK
+adb pull $apkpath apk
+echo Extracting DLLs
 unzip -xfj apk/base.apk "assets/bin/Data/Managed/*.dll" -d apk
+echo linking needed DLLs
 ln -sf apk/Assembly-CSharp-firstpass.dll .
 ln -sf apk/ContentsSerializer.dll .
 ln -sf apk/ProtoBuffSerializer.dll .
 ln -sf apk/protobuf-net.dll .
 ln -sf apk/UnityEngine.dll .
+mkdir -p Databases
 rm -rf Databases/*
-adb pull -a /storage/emulated/0/Android/data/com.sega.PhantasyStarOnline2es/files/3hwQzp8KE9T1oTpJCHPvxI5JIedD3AuT/. Databases
+echo Getting stock Database files
+adb pull -a /sdcard/Android/data/com.sega.PhantasyStarOnline2es/files/3hwQzp8KE9T1oTpJCHPvxI5JIedD3AuT/. Databases
 echo Update patch
 mono ESBreakerCLI.exe > /dev/null
-git commit -a
+export apkversion
+git commit --m "$apkversion" -- json
 ./make.sh
