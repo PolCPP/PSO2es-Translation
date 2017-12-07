9f98dd72539891ebd5d83254c6c50debb9cb7777
Alam Arias <Alam.GBC+git@gmail.com>
2017-12-07 11:22:31 -0500

    Scripts: update.bat almost done, just need unzip via PowerShell

--- update.sh
+++ update.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+git diff --exit-code -- json > /dev/null||echo Unclean JSON folder
+git diff --exit-code -- json > /dev/null||exit
 echo Waiting for android device
 adb wait-for-device
 apkpath=$(adb shell pm path com.sega.PhantasyStarOnline2es | _py/_APK_package.py)
-apkversion=$(adb shell dumpsys package com.sega.PhantasyStarOnline2es| _py/_APK_version.py)
+adb shell dumpsys package com.sega.PhantasyStarOnline2es| _py/_APK_version.py > /tmp/PSO2ver.txt
 mkdir -p apk
 echo pulling PSO2 APK
-adb pull $apkpath apk
+adb shell cp $apkpath /sdcard/PSO2es.zip
+adb pull /sdcard/PSO2es.zip apk/PSO2es.zip
+adb shell rm /sdcard/PSO2es.zip
 echo Extracting DLLs
-unzip -xfj apk/base.apk "assets/bin/Data/Managed/*.dll" -d apk
+unzip -xfj apk/PSO2es.zip "assets/bin/Data/Managed/*.dll" -d apk
 echo linking needed DLLs
 ln -sf apk/Assembly-CSharp-firstpass.dll .
 ln -sf apk/ContentsSerializer.dll .
@@ -18,8 +22,7 @@ mkdir -p Databases
 rm -rf Databases/*
 echo Getting stock Database files
 adb pull -a /sdcard/Android/data/com.sega.PhantasyStarOnline2es/files/3hwQzp8KE9T1oTpJCHPvxI5JIedD3AuT/. Databases
-echo Update patch
+echo Updating JSONs
 mono ESBreakerCLI.exe > /dev/null
-export apkversion
-git commit --m "$apkversion" -- json
-./make.sh
+git commit --file /tmp/PSO2ver.txt -- json
+rem ./make.sh
